67fbdb7bf4771eb8a29fa570e8265910bbc918eb
janhavi <janashinde394@gmail.com>
2022-03-07 02:32:49 +1100

    finished lab successfully

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-function addingEventListener() {
+
+
+    const input = document.getElementById('input');
+
+function addingEventListener ( ){
+    input.addEventListener('click', function () {
+        alert('I was clicked!');
+      });
 }
+addingEventListener();
+
+
+allDivs = document.getElementsByTagName('div');
+const divFive = allDivs[5];
+
+function addingEvListToDiv5 (){
+    divFive.addEventListener('click', function (){
+    alert('Hey There!');
+});
+}
+
+addingEvListToDiv5();
